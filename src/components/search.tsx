66c0ecb2de3a14671723121898ae11bbe92990d2
Sonacho <nachokajita@gmail.com>
2024-08-18 11:58:46 -0300

    error fix

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -2,10 +2,8 @@
 
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-type Props = {
 
-}
-export const Search = ({}: Props) => {
+export const Search = () => {
     return ( 
         <div className="w-full flex justify-end gap-4">
             <Input className="w-400 shadow-sm " placeholder="Search..."/>
