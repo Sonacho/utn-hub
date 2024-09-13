0a85acb29fa66cb429dce45eb6dbfe13c60f0fe9
Sonacho <nachokajita@gmail.com>
2024-09-13 11:53:33 -0300

    fix responsiveness

--- src/app/[id]/[file]/page.tsx
+++ src/app/[id]/[file]/page.tsx
@@ -6,7 +6,7 @@ function getIframeSrc(id:string){
 export default function File ({ params }: { params: { file: string } }) {
 
     return ( 
-        <div className="container flex justify-center">
+        <div className="container h-full flex justify-center">
             <div className="sm:h-full sm:w-full md:w-[400px] md:h-[500px] lg:w-[700px] lg:h-[800px] flex justify-center">
                 <iframe src={getIframeSrc(params.file)} className="w-full h-full" allowFullScreen />
             </div>
