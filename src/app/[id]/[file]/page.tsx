2b058c24eaa1bc6fc9b7bf90c93a413e8a043f62
Sonacho <nachokajita@gmail.com>
2024-09-13 11:47:07 -0300

    fix responsiveness

--- src/app/[id]/[file]/page.tsx
+++ src/app/[id]/[file]/page.tsx
@@ -19,7 +19,7 @@ export default function File ({ params }: { params: { file: string } }) {
     return ( 
         <div className="container flex justify-center">
             <div ref={div} className="sm:h-full sm:w-full md:w-[400px] md:h-[500px] lg:w-[700px] lg:h-[800px] flex justify-center">
-                <iframe src={getIframeSrc(params.file)} width={width} height={height} />
+                <iframe src={getIframeSrc(params.file)} className="w-full h-full" allowFullScreen />
             </div>
         </div>
     );
