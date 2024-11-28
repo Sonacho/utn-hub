d4367b573d874110c29fd4e1c1cc33325407ae86
Sonacho <nachokajita@gmail.com>
2024-11-28 03:56:40 -0300

    generate static params and dynamic metadata/fix breadcrumb

--- src/app/[...id]/layout.tsx
+++ src/app/[...id]/layout.tsx
@@ -1,11 +1,8 @@
 import BreadcrumbComponent from "~/components/breadcrumbComponent";
 
-
 export default async function FoldersLayout({
-    params,
     children
     }: {
-    params: Promise<{ id: string }>,
     children: Readonly<{ children: React.ReactNode}>
     }){
         return(
